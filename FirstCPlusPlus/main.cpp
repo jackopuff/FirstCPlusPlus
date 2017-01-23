ff2dffb119e6ad1f10010398ce40a1350f11e84f
jackopuff <jcar8281@740s-pl-127.local>
2017-01-23 12:21:41 -0700

    Initial Commit

---
+++ FirstCPlusPlus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FirstCPlusPlus
+//
+//  Created by Carter, Jackson on 1/23/17.
+//  Copyright © 2017 Carter, Jackson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
