8f81837846d72941203db9e5e26a2844fb86f1fd
jackopuff <jackson@carterrealm.com>
2017-01-31 13:01:09 -0700

    added method headers to timer, created timer model

--- FirstCPlusPlus/Controller/FirstController.cpp
+++ FirstCPlusPlus/Controller/FirstController.cpp
@@ -6,12 +6,39 @@
 //  Copyright © 2017 Carter, Jackson. All rights reserved.
 //
 
-#include <iostream>
+#include <IOSTREAM>
 #include "FirstController.h"
 
 using namespace std;
-
 void FirstController :: start()
 {
     cout<< "Spaget" <<endl;
+    this->specialOutput();
+}
+
+void FirstController :: specialOutput()
+{
+    cout << "Hello u stinky code user" <<endl;
+    int age;
+    cin >> age;
+    cout << "you typed in " << age << endl;
+    cout << "Type in your name :D" << endl;
+    string name;
+    cin >> name;
+    cout << "your name is " <<name<< " wow cool :O" << endl;
+    cin.ignore();
+    cin>> name;
+    cout << name;
+    cout << "Type your name and age" << endl;
+    cin >> name >> age;
+    cout << "you typed " << name << " and " << age << endl;
+    cout << "type in your full name" << endl;
+    cin.ignore();
+    getline(cin, name);
+    cout << name << endl;
+    string saisstring;
+    cin>> saisstring;
+    cout <<"this is sai's string" << saisstring << endl;
+
+    
 }
